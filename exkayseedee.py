e2b3afea6dd6c1d90f5541255309a4d6413bd072
Hiemanshu Sharma <hiemanshu@gmail.com>
2011-01-02 05:42:55 +0530

    Fixed a small mistake

--- exkayseedee.py
+++ exkayseedee.py
@@ -18,11 +18,12 @@ DOCUMENTATION = ""
 check = os.path.isfile('data.db')
 if (check):
     cdb = ComicsDB('data.db')
+    score = 0
     id = random.randrange(1,840,1) #fix
     alt = cdb.getAlt(id)
     title = cdb.getTitle(id)
     print "Alt Text : %s" %alt
-    t = random.randrange(1,5,1) #fix
+    t = random.randrange(1,6,1) #fix
     ch = 1;
     for i in range(1,6):
         rand = random.randrange(1,840,1) #fix
@@ -30,11 +31,13 @@ if (check):
             if (rand != id):
                 print "%d %s" %(i, cdb.getTitle(rand))
         else:
-            print "%d %s" %(ch, title)
+            print "%d %s" %(t, title)
     while True:
         ans = raw_input('Pick the correct answer : ')
         if (int(ans) == t) :
             print "Answer is correct"
+#            score = + 1;
+#            print "Score : %d" %score
             sys.exit(0)
         else:
             print "Try again"
